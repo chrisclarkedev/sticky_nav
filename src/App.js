17a268fc8d351daf62a8d32d80d6626ac8ff4da8
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-08-29 14:07:39 -0400

    created header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
+      <h1>What is Lorem?</h1>
+      <p>Lorem Ipsum</p>
     </div>
   );
 }
