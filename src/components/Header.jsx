2b8f7914e4964e9b3380993e058c074c553ad5e4
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-08-30 11:49:49 -0400

    added images with styling and lorem content txt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,37 @@ export default function Header() {
   return (
     <div>
       <section class="sec1"> <img src={require('./images/bg1.jpg')} /></section>
-      <section class="sec2"> </section>
-      <section class="sec3"> </section>
-      <section class="sec4"> </section>
-      <section class="sec5"> </section>
+      <section class="content">
+        <h1>What will anyone do without Lorem Ipsum?</h1>
+        <h1>How and why do we use it?</h1>
+        <p>Dolor nisi sunt dolor non consequat do proident. Qui minim voluptate fugiat ea eu minim minim laboris id aliqua Lorem esse anim reprehenderit. Reprehenderit laborum do est ex esse anim eu. Labore esse elit dolor aliqua ex occaecat cupidatat pariatur eu voluptate nulla ea enim. Dolore sit ipsum fugiat pariatur nostrud elit incididunt consectetur. Quis labore exercitation proident nisi eiusmod excepteur adipisicing. Sunt reprehenderit nostrud consequat excepteur cillum commodo magna nisi incididunt velit reprehenderit consequat cupidatat.
+    Nostrud nostrud magna dolor elit pariatur pariatur. Aliquip esse ea aliquip minim nulla ullamco ad. Lorem velit do ea do dolore excepteur nulla.</p>
+        <br></br>
+        <p>Minim id labore nostrud enim culpa deserunt. Amet laboris ut velit nostrud amet. Dolore occaecat commodo amet et incididunt proident Lorem officia aliquip fugiat dolor irure duis labore.
+Ad proident ullamco fugiat ut consectetur tempor adipisicing ex eiusmod. Id ad reprehenderit consequat laboris deserunt ad elit. Enim sint voluptate consequat officia officia ad. Quis nostrud deserunt sunt labore occaecat ad. Non nostrud ut occaecat laborum culpa dolore qui.
+  Duis non amet consectetur eu ipsum reprehenderit incididunt aute aliquip do elit. Eiusmod velit dolore nulla do cupidatat. Officia officia tempor occaecat non fugiat do nulla.</p>
+      </section>
+      <section class="sec2"> <img src={require('./images/bg2.jpg')} /></section>
+      <section class="content">
+        <p>Sunt officia amet qui in. Exercitation laboris nisi pariatur esse incididunt aute occaecat enim fugiat nulla consectetur id proident. Dolore minim et tempor pariatur qui elit ut consectetur fugiat culpa cupidatat.
+  Aute et irure cupidatat est est excepteur proident mollit. Eu irure laborum id fugiat nisi. Excepteur amet veniam deserunt cillum. Ex cillum sunt eiusmod deserunt. Ullamco anim sint ad tempor ullamco aliquip mollit occaecat consequat. Officia ad dolore irure consequat proident nulla amet anim quis mollit.
+  Ipsum sint nostrud ex tempor eiusmod nostrud do commodo Lorem mollit. Proident eiusmod id non do aliquip tempor consectetur sunt reprehenderit cillum nostrud ad. Laborum irure duis et in cupidatat ea nulla duis. Velit laborum magna ex incididunt eiusmod excepteur dolore enim excepteur qui. Eu quis nulla culpa dolore dolor mollit nulla aliquip tempor duis aliquip. Cupidatat anim voluptate pariatur enim est culpa minim amet ex et.
+  Officia anim incididunt irure et aliquip in aliquip aliqua ullamco duis. Officia officia deserunt ex labore officia amet. Cillum enim ea pariatur ut est nostrud duis officia amet dolore. Proident sunt irure ullamco eiusmod ea sit adipisicing velit eu et occaecat commodo. Veniam laborum dolore ut duis aliquip Lorem. Incididunt quis amet et nostrud enim nisi reprehenderit.
+  Occaecat commodo sunt enim deserunt irure culpa minim veniam deserunt. Anim nisi dolor enim culpa aliqua magna exercitation irure consequat enim. Magna voluptate culpa tempor eu labore est exercitation.
+  Minim adipisicing cillum voluptate fugiat elit laboris consectetur sunt mollit reprehenderit eu. Incididunt ea reprehenderit id id pariatur reprehenderit sint ex ad velit incididunt aute. Minim incididunt irure Lorem occaecat occaecat deserunt sit pariatur. Adipisicing commodo consectetur commodo dolore mollit pariatur. Qui proident nulla veniam culpa esse nostrud magna sit est magna fugiat reprehenderit pariatur. Reprehenderit ullamco qui commodo in consectetur fugiat.
+Nisi ullamco officia proident reprehenderit nulla elit est id irure cupidatat eiusmod. Occaecat aliqua ad culpa exercitation esse aliquip consectetur nisi ipsum Lorem. Officia ullamco dolor occaecat eu magna excepteur ea. Tempor reprehenderit deserunt velit do velit ipsum veniam.</p>
+      </section>
+      <section class="sec3"> <img src={require('./images/bg1.jpg')} /></section>
+      <section class="content">
+        <p>Labore officia consectetur aliquip irure eu labore consequat duis cupidatat. Nostrud officia nisi nulla deserunt. Amet anim reprehenderit nostrud pariatur. Commodo ipsum eu consequat eu pariatur eu officia ad occaecat. Consequat sit aute proident fugiat adipisicing et et ad. Veniam culpa do labore fugiat id.
+  Veniam aliquip pariatur duis reprehenderit minim commodo eu aliquip nulla ut sunt velit. Labore labore in esse veniam proident duis culpa. Eu dolor et cupidatat cupidatat id anim veniam. Irure adipisicing dolore occaecat sint tempor.
+  Veniam nostrud mollit magna excepteur aliqua velit aute cupidatat consectetur sit aute Lorem sit. Consectetur irure consequat anim tempor. Laboris quis ut veniam nostrud adipisicing do. Dolor laborum laboris enim duis velit aute duis. Minim nostrud deserunt ut qui do.
+  Irure et esse sunt id et esse. Ad esse minim commodo laboris veniam consectetur et officia. Amet eu anim sit ipsum sint aliqua reprehenderit id Lorem. Quis laborum mollit consectetur excepteur amet consectetur. Do incididunt nisi quis velit anim aliqua ipsum excepteur nulla exercitation et et esse.
+  Elit irure excepteur veniam in aliqua nulla do dolor eu cupidatat voluptate duis. Nostrud ipsum est qui ipsum. Adipisicing labore dolor id irure non. Non fugiat dolor aliquip officia aliquip eiusmod ipsum ullamco nostrud non. Anim laboris exercitation ex adipisicing amet quis non laboris. Ut aute et sunt proident duis ipsum irure cupidatat dolor consectetur. Cillum ut nostrud elit ea proident.
+  Sint deserunt duis tempor enim dolor nisi ad. Commodo minim officia nisi incididunt ea reprehenderit sunt enim ipsum. In incididunt elit excepteur cupidatat do elit tempor magna pariatur magna commodo elit. Ipsum eiusmod laborum ipsum qui culpa. Ipsum duis sunt quis ex in non ullamco exercitation sunt quis laboris sunt eiusmod.
+  Cupidatat velit et ea enim reprehenderit. Non sit mollit elit dolore do culpa proident. Non non nisi id sint deserunt adipisicing dolor cupidatat.
+Adipisicing eiusmod dolore velit pariatur do incididunt ex laboris aliqua nisi consequat amet ea minim. Pariatur et esse consequat anim duis nisi dolor duis veniam. Deserunt consequat commodo sint incididunt esse.</p>
+      </section>
     </div>
   )
 }
